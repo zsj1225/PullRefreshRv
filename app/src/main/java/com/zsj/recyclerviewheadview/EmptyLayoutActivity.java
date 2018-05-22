f8a2cd6a2c364f360b17ea78531c4e301dc86b64
zhushengjun <shengjun.zhu1@itel-mobile.com>
2018-05-22 21:20:17 +0800

    空布局下拉 +2

--- app/src/main/java/com/zsj/recyclerviewheadview/EmptyLayoutActivity.java
+++ app/src/main/java/com/zsj/recyclerviewheadview/EmptyLayoutActivity.java
@@ -1,9 +1,11 @@
 package com.zsj.recyclerviewheadview;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Toast;
 
 public class EmptyLayoutActivity extends AppCompatActivity {
 
@@ -19,6 +21,14 @@ public class EmptyLayoutActivity extends AppCompatActivity {
         mWrapEmptyView.addRefreshViewCreator(defaultRefreshCreator);
         View emptyView = LayoutInflater.from(this).inflate(R.layout.layout_empty, mWrapEmptyView, false);
         mWrapEmptyView.addEmptyView(emptyView);
+        mWrapEmptyView.setOnRefreshListener(new WrapRecyclerView.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Toast.makeText(EmptyLayoutActivity.this, "onRefresh", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(EmptyLayoutActivity.this,NewActivity.class));
+                mWrapEmptyView.onStopPullRefresh();
+            }
+        });
 
     }
 }
